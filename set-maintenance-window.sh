24d092bffaebc580b8eb4417a62ef5906cb23701
SongGuoLiang <130327608+livesRan@users.noreply.github.com>
2026-09-16 09:40:57 +0800

    Delete set-maintenance-window.sh

--- set-maintenance-window.sh
+++
@@ -1,5 +0,0 @@
-# 设置维护时间窗为周一凌晨 01:00 至 02:00 (时区 UTC+8)
-az mysql flexible-server update \
-  --resource-group "rg-tssnkur-prod" \
-  --name "tssnkur-mysql-prod" \
-  --maintenance-window day-of-week=1 hour=1 minute=0
